9a234430f8afac4b6afffc7cd442de3b1634d428
Thales Aguiar <thalesaguiar21@gmail.com>
2019-05-21 13:03:11 -0300

    Cleaned up processing functions

--- pyspeech/processing.py
+++ pyspeech/processing.py
@@ -1,40 +1,16 @@
 import numpy as np
 import math
-import pyspeech.folder as spfold
 import pyspeech.transform as sptrans
-from scipy.io import wavfile
 
 
-def powerspectrum(datapath, emphasis, frame_size, frame_stride):
-    windowed_signal = window_voice_dataset(
-        datapath, emphasis, frame_size, frame_stride)
-    return sptrans.fft(windowed_signal, 512)
+def powerspectrum(signal, freq, frame_size, frame_stride, gain):
+    emph_signal = emphasize(signal)
+    framed_signal = make_frames(emph_signal, freq, frame_size, frame_stride)
+    windowed_frames = _hamming_window(framed_signal, frame_size)
+    return sptrans.fft(windowed_frames, 512)
 
 
-def window_voice_dataset(data_path, emph_rate, frame_size, frame_stride):
-    audio_files_path = spfold.find_wav_files(data_path)
-    onfreq_signals = []
-    for audio_path in audio_files_path:
-        onfreq_signals.append(
-            windowed_signal(audio_path, emph_rate, frame_size, frame_stride)
-        )
-    return onfreq_signals
-
-
-def windowed_signal(audio, emph_rate, frame_size, frame_stride):
-    rate, signal = wavfile.read(audio)
-    emph_signal = _preemph(signal, emph_rate)
-    frames = _split(rate, emph_signal, frame_size, frame_stride)
-    frame_length = frames.shape[1]
-    _hamming_window(frames, frame_length)
-    return frames
-
-
-def _preemph(signal, alpha):
-    return np.append(signal[0], signal[1:] - alpha * signal[:-1])
-
-
-def _split(sample_rate, signal, frame_size, frame_stride):
+def make_frames(signal, sample_rate, frame_size, frame_stride):
     frame_length = int(round(frame_size/1000.*sample_rate))  # miliseconds
     frame_step = int(round(frame_stride/1000.*sample_rate))  # miliseconds
     signal_length = len(signal)
@@ -55,5 +31,5 @@ def _hamming_window(frames, length):
     frames *= 0.54 - 0.46*math.cos(2.0*math.pi/(length))
 
 
-def equal_loudness_preemphasis():
-    pass
+def emphasize(signal, gain):
+    return np.append(signal[0], signal[1:] - gain*signal[:-1])
