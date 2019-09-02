84a144fb054eabde7b64d4c6734dabfb4b2e82bc
Thales Aguiar <thalesaguiar21@gmail.com>
2019-09-02 08:55:41 -0300

    Find wav files is no longer case sensitive

--- pyspeech/folder.py
+++ pyspeech/folder.py
@@ -3,9 +3,10 @@ import os
 
 def _find_files(folder_path, extension):
     dt_files = []
-    for file in os.listdir(folder_path):
-        if file.endswith("." + extension):
-            dt_files.append(os.path.join(folder_path, file))
+    for file_ in os.listdir(folder_path):
+        if file_.lower().endswith("." + extension):
+            dt_files.append(os.path.join(folder_path, file_))
+    print(f"Found {len(dt_files)} wav files!")
     return dt_files
 
 
