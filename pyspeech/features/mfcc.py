2e8036a7fbc3e327343ddca839180b28c1013b8f
Thales Aguiar <thalesaguiar21@gmail.com>
2019-09-04 08:56:28 -0300

    Encapsulated mfcc, delta, and double delta means into a single module function

--- pyspeech/features/mfcc.py
+++ pyspeech/features/mfcc.py
@@ -2,6 +2,19 @@ import numpy as np
 import pyspeech.dsp.processing as spproc
 import pyspeech.dsp.filters as spfilt
 import scipy.fftpack as scifft
+import pyspeech.features.dynamics as spdyn
+
+
+def make_means_and_deltas(signals, frequencies, nfilt, processor, cepstrums):
+    ''' Extract MFCC cepstrums, delta, and double delta '''
+    mfccs = extract(signals, frequencies, nfilt, processor, cepstrums)
+    mfcc_means = np.array([np.mean(mfcc, axis=0) for mfcc in mfccs])
+    delta = spdyn.Delta(smooth=2)
+    delta1 = [delta.make(mfcc) for mfcc in mfccs]
+    delta1means = np.array([delta.make_means(mfcc) for mfcc in mfccs])
+    delta2means = np.array([delta.make_means(dt1.T) for dt1 in delta1])
+    allfeats = np.hstack((mfcc_means, delta1means, delta2means))
+    return allfeats
 
 
 def extract(signals, frequencies, nfilt, processor, cepstrums=13):
@@ -25,7 +38,8 @@ def _fix_dimensions(signals, frequencies):
 def _mfcc(signal, frequency, nfilt, processor, cepstrums):
     # Applies a Discrete Cosine Tranform (DCT) on Filter Banks
     power_spec = processor.preprocess(signal, frequency)
-    filtered_frames = spfilt.mel_banks(power_spec, nfilt, frequency, processor.NFFT)
+    filtered_frames = spfilt.mel_banks(power_spec, nfilt,
+                                       frequency, processor.NFFT)
     dctframes = scifft.dct(filtered_frames, type=2, axis=1, norm='ortho')
     mfccs = np.array(dctframes)[:, 1:cepstrums + 1]
     return mfccs
