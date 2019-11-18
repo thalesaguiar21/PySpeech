c51d00bf0189000c066d82ad15ac305c15d598e7
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-18 10:21:56 -0300

    Updated spectrum functions to use configuration file

--- pyspeech/dsp/processing.py
+++ pyspeech/dsp/processing.py
@@ -62,25 +62,25 @@ class Signal:
         self.samplerate= freq
 
 
-def norm_log_power_spectrum(signal, frame, nfft):
-    log_spec = _log_power_spectrum(signal, frame, nfft)
+def norm_log_power_spectrum(signal):
+    log_spec = _log_power_spectrum(signal)
     return log_spec - np.max(log_spec)
 
 
 def log_power_spectrum(signal, frame, nfft):
-    pow_spec = _power_spectrum(signal, frame, nfft)
+    pow_spec = _power_spectrum(signal)
     bounded_psec = np.fmax(pow_spec, np.finfo(np.float64).eps)
     log_spec = np.log10(bounded_psec)
 
 
-def power_spectrum(signal, frame, nfft):
-    mag_spec = _mag_spectrum(signal, frame, nfft)
+def power_spectrum(signal):
+    mag_spec = _mag_spectrum(signal)
     return 1.0/nfft * mag_spec**2
 
 
-def mag_spectrum(signal, frame, nfft):
-    wnd_amps = _split(signal, frame)
-    spectrum = np.fft.rfft(frames, nfft)
+def mag_spectrum(signal):
+    wnd_amps = _split(signal)
+    spectrum = np.fft.rfft(frames, confs['nfft'])
     return np.absolute(spectrum)
 
 
