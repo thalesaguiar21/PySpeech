361064ab51e1d33229a2107de3d794f62ca7d271
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-18 10:30:35 -0300

    removed spectrum suffix from spectrum module functions

--- pyspeech/dsp/spectrum.py
+++ pyspeech/dsp/spectrum.py
@@ -3,23 +3,23 @@ import numpy as np
 from pyspeech.configs import confs
 
 
-def norm_log_power_spectrum(signal):
+def norm_log_power(signal):
     log_spec = _log_power_spectrum(signal)
     return log_spec - np.max(log_spec)
 
 
-def log_power_spectrum(signal, frame, nfft):
+def log_power(signal, frame, nfft):
     pow_spec = _power_spectrum(signal)
     bounded_psec = np.fmax(pow_spec, np.finfo(np.float64).eps)
     log_spec = np.log10(bounded_psec)
 
 
-def power_spectrum(signal):
+def power(signal):
     mag_spec = _mag_spectrum(signal)
     return 1.0/nfft * mag_spec**2
 
 
-def mag_spectrum(signal):
+def magnitude(signal):
     wnd_amps = _split(signal)
     spectrum = np.fft.rfft(frames, confs['nfft'])
     return np.absolute(spectrum)
