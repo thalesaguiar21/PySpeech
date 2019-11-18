5a13f0a3fff134479fc934a5cd2c7afc5c87a82e
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-18 10:26:51 -0300

    Removed processor class

--- pyspeech/dsp/processing.py
+++ pyspeech/dsp/processing.py
@@ -9,37 +9,6 @@ import numpy as np
 from pyspeech.configs import confs
 
 
-class Processor:
-    """ """
-    def __init__(self, frame, emph, nfft):
-        self.frame = frame
-        self.emph = emph
-        self.NFFT = nfft
-
-    def preprocess(self, signal, freq):
-        emph_signal = emphasize(signal, self.emph)
-        framed_signal = self.make_frames(emph_signal, freq)
-        framed_signal *= np.hamming(self.frame.length(freq))
-        return sptransf.make_power_spectrum(framed_signal, self.NFFT)
-
-    def make_frames(self, signal, sample_rate):
-        frame_length = self.frame.length(sample_rate)
-        frame_step = self.frame.step(sample_rate)
-        signal_length = len(signal)
-        qtd_frames = math.ceil((signal_length - frame_length) / frame_step)
-
-        pad_signal_length = qtd_frames*frame_step + frame_length
-        z = np.zeros(pad_signal_length - signal_length)
-        padded_signal = np.append(signal, z)
-
-        frame_begin = np.tile(np.arange(0, frame_length), (qtd_frames, 1))
-        frm_end = np.tile(
-            np.arange(0, qtd_frames * frame_step, frame_step),
-            (frame_length, 1)).T
-        indices = frame_begin + frm_end
-        return padded_signal[indices.astype(np.int32, copy=False)]
-
-
 def frame_len(size, freq):
     return int(round(size/1000. * freq))
 
