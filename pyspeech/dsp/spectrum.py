12644b3c767cc789e4c9c1c406ef140d1291b98c
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-19 09:58:20 -0300

    Fixed old variable names and added verification to split or not the signal

--- pyspeech/dsp/spectrum.py
+++ pyspeech/dsp/spectrum.py
@@ -1,26 +1,33 @@
 import numpy as np
 
 from pyspeech.configs import confs
+import pyspeech.dsp.processing as sp
 
 
 def norm_log_power(signal):
-    log_spec = _log_power_spectrum(signal)
+    log_spec = log_power(signal)
     return log_spec - np.max(log_spec)
 
 
 def log_power(signal, frame, nfft):
-    pow_spec = _power_spectrum(signal)
+    pow_spec = power(signal)
     bounded_psec = np.fmax(pow_spec, np.finfo(np.float64).eps)
     log_spec = np.log10(bounded_psec)
 
 
 def power(signal):
-    mag_spec = _mag_spectrum(signal)
-    return 1.0/nfft * mag_spec**2
+    mag_spec = magnitude(signal)
+    return 1.0/confs['nfft'] * mag_spec**2
 
 
 def magnitude(signal):
-    wnd_amps = _split(signal)
-    spectrum = np.fft.rfft(frames, confs['nfft'])
+    wnd_frames = make_frame(signal)
+    spectrum = np.fft.rfft(wnd_frames, confs['nfft'])
     return np.absolute(spectrum)
+
+def make_frame(signal):
+    if len(signal.shape) == 2:
+        return signal
+    else:
+        return sp.split_with_stride(signal)
 
