a1bd9db58fe3e1f62f6bac10c9064e34ba31cc3a
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-19 10:43:49 -0300

    Finished mfcc refactor

--- pyspeech/features/mfcc.py
+++ pyspeech/features/mfcc.py
@@ -7,13 +7,19 @@ import pyspeech.dsp.metrics as smet
 from pyspeech.configs import confs
 
 
-def _extract(signal, nfilt, ncep, highfreq, lowfreq, emph):
+def _extract(signal, nfilt, ncep, emph, ceplift, lowfreq, highfreq=None):
     wnd_signal = make_frames_and_spectrum(signal, emph)
     power_spectrum = spec.power(wnd_signal)
     filter_banks = make_filter_banks(power_spectrum, highfreq, lowfreq,
                                      nfilt, signal.samplerate)
 
     fbanks_energies = power_spectrum @ filter_banks.T
+    # Prevent zero for log
+    fbanks_energies_cut = np.fmax(fbanks_energies, np.finfo(np.float64).eps)
+    fbanks_log = np.log(fbanks_energies_cut)
+    cepstrums = scifft.dct(fbanks_log, type=2, axis=1, norm='ortho')[:, :ncep]
+    lifted_cepstrums = lifter(cepstrums, ceplift)
+    return lifted_cepstrums
 
 
 def make_frames_and_spectrum(signal, emph):
@@ -39,40 +45,12 @@ def make_filter_banks(power_spec, highfreq, lowfreq, nfilt, srate):
     return fbanks
 
 
-def make_means_and_deltas(signals, frequencies, nfilt, processor, cepstrums):
-    ''' Extract MFCC cepstrums, delta, and double delta '''
-    mfccs = extract(signals, frequencies, nfilt, processor, cepstrums)
-    mfcc_means = np.array([np.mean(mfcc, axis=0) for mfcc in mfccs])
-    delta = spdyn.Delta(smooth=2)
-    d1, d2 = delta.make_delta_and_ddelta_means(mfccs)
-    allfeats = np.hstack((mfcc_means, d1, d2))
-    return allfeats
-
-
-def extract(signals, frequencies, nfilt, processor, cepstrums=13):
-    signals, frequencies = _fix_dimensions(signals, frequencies)
-    mfccs = []
-    for signal, frequency in zip(signals, frequencies):
-        mfccs.append(_mfcc(signal, frequency, nfilt, processor, cepstrums))
-    return mfccs
-
-
-def _fix_dimensions(signals, frequencies):
-    augmented_signal = signals
-    augmented_freqs = frequencies
-    if not isinstance(signals[0], (list, np.ndarray)):
-        augmented_signal = [signals]
-    if not isinstance(frequencies, list):
-        augmented_freqs = [frequencies]
-    return augmented_signal, augmented_freqs
-
-
-def _mfcc(signal, frequency, nfilt, processor, cepstrums):
-    # Applies a Discrete Cosine Tranform (DCT) on Filter Banks
-    power_spec = processor.preprocess(signal, frequency)
-    filtered_frames = spfilt.mel_banks(power_spec, nfilt,
-                                       frequency, processor.NFFT)
-    dctframes = scifft.dct(filtered_frames, type=2, axis=1, norm='ortho')
-    mfccs = np.array(dctframes)[:, 1:cepstrums + 1]
-    return mfccs
+def lifter(cepstrums, l=22):
+    if l > 0:
+        nframes, ncoefs = cepstrums.shape
+        n = np.arange(ncoefs)
+        lift = 1 + (l/2)*np.sin(np.pi*n / l)
+        return lift * cepstrums
+    else:
+        return cepstrums
 
--- tests/tests_mfcc.py
+++ tests/tests_mfcc.py
@@ -16,17 +16,17 @@ class TestsMFCC(unittest.TestCase):
         self.lowfreq = 300
         self.emph = 0.97
         self.signnal = sp.Signal([], 0)
+        self.ceplift = 22
 
     def test_signal_200l_20hz(self):
         self.make_signal(32000, 8000)
         self.extract()
-
 
     def make_signal(self, slen, samplerate):
         amps = np.arange(slen)
         self.signal = sp.Signal(amps, samplerate)
 
     def extract(self):
-        mfcc._extract(self.signal, self.nfilt, self.ncep, self.highfreq,
-                      self.lowfreq, self.emph)
+        mfcc._extract(self.signal, self.nfilt, self.ncep, self.emph,
+                      self.ceplift, self.lowfreq, self.highfreq)
 
