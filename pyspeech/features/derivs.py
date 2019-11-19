6f71d7e6ccbab9558d0960495cc5306d77854d8a
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-19 11:09:27 -0300

    Fixed delta dimension and denominator

--- pyspeech/features/derivs.py
+++ pyspeech/features/derivs.py
@@ -4,6 +4,7 @@ import scipy.fftpack as scifft
 import pyspeech.dsp.processing as spproc
 import pyspeech.dsp.filters as spfilt
 
+
 def delta(feats, smooth=2):
     """ Extract deltas from the frames
 
@@ -11,10 +12,10 @@ def delta(feats, smooth=2):
         frames (ndarray): The feature matrix
         smooth (int): The number frames to jump
     """
-    denom = 2/6 * (smooth+1) * (2*smooth + 1)
+    denom = 2/6 * smooth * (smooth+1) * (2*smooth + 1)
     nframes, fdim = feats.shape
     pad_feats = np.pad(feats, ((smooth, smooth), (0, 0)), mode='edge')
-    deltas = np.zeros((nframes, fdim - smooth))
+    deltas = np.zeros(feats.shape)
     for t in range(nframes):
         coefs = np.arange(-smooth, smooth + 1)
         deltas[t] = coefs @ pad_feats[t:t + 2*smooth + 1]
