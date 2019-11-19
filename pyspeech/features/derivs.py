dfa82e1a2bd0c7609e0789bdb75b150718af0ae9
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-19 10:56:01 -0300

    Improved delta computing

--- pyspeech/features/derivs.py
+++ pyspeech/features/derivs.py
@@ -4,21 +4,21 @@ import scipy.fftpack as scifft
 import pyspeech.dsp.processing as spproc
 import pyspeech.dsp.filters as spfilt
 
-def delta(frames, smooth=2):
+def delta(feats, smooth=2):
     """ Extract deltas from the frames
 
     Args:
         frames (ndarray): The feature matrix
         smooth (int): The number frames to jump
     """
-    frames_t = frames.T
     denom = 2/6 * (smooth+1) * (2*smooth + 1)
-    deltas = np.zeros((frames_t.shape[0], frames_t.shape[1] - smooth))
-    for frm, delta in zip(frames_t, deltas):
-        for t in range(frames_t.shape[0] - smooth):
-            for n in range(1, smooth + 1):
-               delta[t] += n*(frm[t+n] - frm[t-n]) / denom
-    return deltas
+    nframes, fdim = feats.shape
+    pad_feats = np.pad(feats, ((smooth, smooth), (0, 0)), mode='edge')
+    deltas = np.zeros((nframes, fdim - smooth))
+    for t in range(nframes):
+        coefs = np.arange(-smooth, smooth + 1)
+        deltas[t] = coefs @ pad_feats[t:t + 2*smooth + 1]
+    return deltas / denom
 
 
 def log_energy(signal, emph, frame, lfreq, hfreq):
