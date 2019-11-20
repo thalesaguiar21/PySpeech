0b5596ab1dd4347743c4103cff0782aae804e976
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-20 09:52:59 -0300

    Added abstraction for MFCC and MelFilter as dataclasses

--- pyspeech/features/mfcc.py
+++ pyspeech/features/mfcc.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import numpy as np
 import scipy.fftpack as scifft
 
@@ -8,109 +10,30 @@ import pyspeech.features.derivs as sder
 from pyspeech.configs import confs
 
 
-class Extractor:
-
-    def __init__(self, melfilter, ncep=13, lift=22):
-        self.powspec = np.array([])
-        self.ncep = ncep
-        self.melfilter = melfilter
-        self.lift = lift
-
-    def extract(signal, emph):
-        self.powsepc = _make_power_spectrum(signal, emph)
-        if confs['append_energy']:
-            feats = self.mfcc_and_energy(power_spectrum)
-        else:
-            feats = self.mfcc(power_spectrum)
-        return feats
-
-    def mfcc_and_energy(self):
-        mfccs = self.mfcc()
-        energies = sder.log_energy(self.powspec)
-        return np.hstack((energies.reshape(energies.size, 1), mfccs))
-
-    def mfcc(self):
-        filter_banks = self.melfilter.make()
-        fbanks_energies = self.powspec @ filter_banks.T
-        bound_energies = np.fmax(fbanks_energies, np.finfo(np.float64).eps)
-        fbanks_log = np.log(bound_energies)
-        cepstrums = scifft.dct(fbanks_log, type=2, axis=1, norm='ortho')
-        cepstrum_cut = cepstrums[:, :self.ncep]
-        lifted_cepstrums = lifter(cepstrums_cut, self.ceplift)
-        return lifted_cepstrums
-
-
-class MelFilter:
-
-    def __init__(self, nfilt, highfreq, lowfreq):
-        self.nfilt = nfilt
-        self.highfreq = highfreq if highfreq is not None else srate/2
-        self.lowfreq = lowfreq
-
-    def make(self):
-        bins = self.bins()
-        fbanks = self.fbanks(bins)
-        return fbanks
-
-    def bins(self):
-        hihgmel = smet.hz_to_mel(self.highfreq)
-        lowmel = smet.hz_to_mel(self.lowfreq)
-        mel_points = np.linspace(lowmel, highmel, nfilt + 2)
-        bins = np.floor((confs['nfft'] + 1)*smet.mel_to_hz(mel_points) / srate)
-        return bins
-
-    def fbanks(self, bins):
-        fbanks = np.zeros((self.nfilt, confs['nfft']//2 + 1))
-        for j in range(self.nfilt):
-            for i in range(int(bins[j]), int(bins[j + 1])):
-                fbanks[j, i] = (i-bins[j]) / (bins[j + 1]-bins[j])
-            for i in range(int(bins[j + 1]), int(bins[j + 2])):
-                fbanks[j, i] = (bins[j + 2]-i) / (bins[j + 2]-bins[j + 1])
-        return fbanks
-
-
-@dataclass
-class MFCC:
-    ncep: int
-    lift: int
-    powspec: np.ndarray
-
-
-@dataclass
-class MelFilter:
-    nfilt: int
-    highfreq: float
-    lowfreq: float
-
-
 def extract(signal, mfcc, melfilter, emph):
     powspec = _make_power_spectrum(signal, emph)
+    srate = signal.samplerate
     if confs['append_energy']:
-        feats = _extract_mfcc_and_energy(powspec, mfcc, melfilter, emph,
-                                         signal.samplerate)
+        feats = _extract_mfcc_and_energy(powspec, mfcc, melfilter, srate)
     else:
-        feats = _extract_mfcc(powspec, nfilt, ncep, emph, ceplift,
-                              signal.samplerate, lowfreq, highfreq)
+        feats = _extract_mfcc(powspec, mfcc, melfilter, srate)
     return feats
 
 
-def _extract_mfcc_and_energy(powspec, nfilt, ncep, emph, ceplift, srate,
-                             lowfreq, highfreq=None):
-    mfccs = _extract_mfcc(powspec, nfilt, ncep, ceplift, srate,
-                          lowfreq, highfreq)
+def _extract_mfcc_and_energy(powspec, mfcc, melfilter, srate):
+    mfccs = _extract_mfcc(powspec, mfcc, melfilter, srate)
     energies = sder.log_energy(powspec)
     return np.hstack((energies.reshape(energies.size, 1), mfccs))
 
 
-def _extract_mfcc(powspec, nfilt, ncep, emph, ceplift, srate,
-                  lowfreq, highfreq=None):
-    filter_banks = make_filter_banks(powspec, highfreq, lowfreq, nfilt, srate)
+def _extract_mfcc(powspec, mfcc, melfilter, srate):
+    filter_banks = make_filter_banks(powspec, melfilter, srate)
     fbanks_energies = powspec @ filter_banks.T
-    # Prevent zero for log
     fbanks_energies_cut = np.fmax(fbanks_energies, np.finfo(np.float64).eps)
     fbanks_log = np.log(fbanks_energies_cut)
-    cepstrums = scifft.dct(fbanks_log, type=2, axis=1, norm='ortho')[:, :ncep]
-    lifted_cepstrums = lifter(cepstrums, ceplift)
+    cepstrums = scifft.dct(fbanks_log, type=2, axis=1, norm='ortho')
+    cepstrums_cut = cepstrums[:, :mfcc.ncep]
+    lifted_cepstrums = lifter(cepstrums_cut, mfcc.lift)
     return lifted_cepstrums
 
 
@@ -126,20 +49,35 @@ def make_frames_and_window(signal, emph):
     return ham_frames
 
 
-def make_filter_banks(power_spec, highfreq, lowfreq, nfilt, srate):
-    highfreq = highfreq if highfreq is not None else srate/2
-    highmel = smet.hz_to_mel(highfreq)
-    lowmel = smet.hz_to_mel(lowfreq)
-    mel_points = np.linspace(lowmel, highmel, nfilt + 2)
+def make_filter_banks(power_spec, melfilter, srate):
+    if melfilter.highfreq is None:
+        melfilter.highfreq = srate/2
+    highmel = smet.hz_to_mel(melfilter.highfreq)
+    lowmel = smet.hz_to_mel(melfilter.lowfreq)
+    mel_points = np.linspace(lowmel, highmel, melfilter.nfilt + 2)
     bins = np.floor((confs['nfft'] + 1)*smet.mel_to_hz(mel_points) / srate)
 
-    fbanks = np.zeros((nfilt, confs['nfft']//2 + 1 ))
-    for j in range(nfilt):
+    fbanks = np.zeros((melfilter.nfilt, confs['nfft']//2 + 1 ))
+    for j in range(melfilter.nfilt):
         for i in range(int(bins[j]), int(bins[j + 1])):
             fbanks[j, i] = (i-bins[j]) / (bins[j + 1]-bins[j])
         for i in range(int(bins[j + 1]), int(bins[j + 2])):
             fbanks[j, i] = (bins[j + 2]-i) / (bins[j + 2]-bins[j + 1])
     return fbanks
+
+
+@dataclass
+class MFCC:
+    ncep: int
+    lift: int
+    powspec: np.ndarray = np.array([])
+
+
+@dataclass
+class MelFilter:
+    nfilt: int
+    highfreq: float
+    lowfreq: float
 
 
 def lifter(cepstrums, l=22):
--- tests/tests_mfcc.py
+++ tests/tests_mfcc.py
@@ -6,17 +6,13 @@ import pyspeech.dsp.processing as sp
 import pyspeech.features.mfcc as mfcc
 from pyspeech.configs import confs
 
-
 class TestsMFCC(unittest.TestCase):
 
     def setUp(self):
-        self.nfilt = 40
-        self.ncep = 13
-        self.highfreq = 4000
-        self.lowfreq = 300
+        self.mfcc = mfcc.MFCC(13, 22)
+        self.mfilter = mfcc.MelFilter(40, 4000, 300)
         self.emph = 0.97
         self.signnal = sp.Signal([], 0)
-        self.ceplift = 22
 
     def test_signal_200l_20hz(self):
         self.make_signal(32000, 8000)
@@ -27,6 +23,5 @@ class TestsMFCC(unittest.TestCase):
         self.signal = sp.Signal(amps, samplerate)
 
     def extract(self):
-        mfcc.extract(self.signal, self.nfilt, self.ncep, self.emph,
-                     self.ceplift, self.lowfreq, self.highfreq)
+        mfcc.extract(self.signal, self.mfcc, self.mfilter, self.emph)
 
