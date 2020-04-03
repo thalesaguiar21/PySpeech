834096dec59493ea02b7b2dd29de25c8579f9bd6
Thales Aguiar <thalesaguiar21@gmail.com>
2020-04-03 16:18:46 -0300

    Added method to split by a portion of the shortest segment

--- pyspeech/data/oversample.py
+++ pyspeech/data/oversample.py
@@ -5,10 +5,15 @@ import numpy as np
 from ..dsp import processing as sproc
 
 
-def split_by_shorter(signals, fs):
+def by_shortest(signals, fs):
+    splits = by_scalar_shortest(signals, fs, 1)
+    return splits
+
+
+def by_scalar_shortest(signals, fs, alpha=3):
     shortest = _find_shortest(signals)
-    print(f"Shorter signal: {shortest.size / fs}")
-    splits = _split_all(signals, shortest.size)
+    samplesize = math.ceil(shortest.size / alpha)
+    splits = _split_all(signals, samplesize)
     return splits
 
 
@@ -18,43 +23,6 @@ def _find_shortest(signals):
         if signal.size < shortest.size:
             shortest = signal
     return shortest
-
-
-def by_segment(signals, fs):
-    segments = []
-    for signal in signals:
-        sig_segments = _voiced_segments(signals[0], fs)
-        segments.extend(sig_segments)
-    padded_segments = _fill_with_zeros(segments)
-    return np.array(padded_segments)
-
-
-def _voiced_segments(signal, fs, sil_thres=1e-2):
-    normsignal = sproc.normalise(sproc.Signal(signal, fs))
-    segments = []
-    segment = []
-    for amp in normsignal.amps:
-        if abs(amp) > sil_thres:
-            segment.append(amp)
-        elif len(segment) > 0:
-            segments.append(segment[:])
-            segment.clear()
-    return segments
-
-
-def _fill_with_zeros(segments):
-    largest = segments[0]
-    for segment in segments[1:]:
-        if len(segment) > len(largest):
-            largest = segment
-
-    zerofilled = []
-    largest_size = len(largest)
-    for segment in segments:
-        padlen = largest_size - len(segment)
-        padded_seg = np.append(segment, np.zeros(padlen))
-        zerofilled.append(padded_seg)
-    return zerofilled
 
 
 def _split_all(signals, nsamples):
