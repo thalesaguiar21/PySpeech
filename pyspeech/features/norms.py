8a49a590c5489e13f0953e5e912660111781a713
Thales Aguiar <thalesaguiar21@gmail.com>
2020-04-20 17:17:38 -0300

    improved 'norms' modeule interface

--- pyspeech/features/norms.py
+++ pyspeech/features/norms.py
@@ -1,35 +1,35 @@
 import numpy as np
 
 
-def normalise_by_column(dataset, metric_='minmax'):
+def apply_at_col(dataset, metric_='minmax'):
     normalise(dataset.T, metric_)
 
 
-def normalise(dataset, metric='minmax'):
+def apply(dataset, metric='minmax'):
     for j in range(dataset.shape[0]):
         normalised_row = normalise_by(metric, dataset[j])
         dataset[j] = normalised_row
 
 
-def normalise_by(metric, sample):
+def _normalise_by(metric, sample):
     if metric == 'minmax':
-        return minmax(sample)
+        return _minmax(sample)
     elif metric == 'stdscore':
-        return std_score(sample)
+        return _std_score(sample)
     else:
         print('unknown normalisation')
 
 
-def std_score(x):
-    mean = x.mean()
-    std = x.std()
-    return (x - mean) / std
-
-
-def minmax(x):
+def _minmax(x):
     min_ = x.min()
     max_ = x.max()
     return (x-min_) / (max_-min_)
+
+
+def _std_score(x):
+    mean = x.mean()
+    std = x.std()
+    return (x - mean) / std
 
 
 def mean_normalise(feature):
