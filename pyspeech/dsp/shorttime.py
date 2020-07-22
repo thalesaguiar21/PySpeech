Fixed short-time computation to use samples from previous frames

pyspeech/dsp/shorttime.py
@@ -4,11 +4,14 @@ from . import frame
 
 
 def log_energy(frames):
+    """ Computes the normalised short-time energy of the given frames """
     energies = energy(frames)
-    return 10 * np.log10(1e-5 + energies)
+    bounded_egys = np.fmax(energies, np.finfo(np.float64).eps)
+    return 10 * np.log10(bounded_egys)
 
 
 def energy(frames):
+    """ Computes the short-time energy """
     fixedframes = _fix_frames(frames)
     __, flen = fixedframes.shape
     wnd_frames = fixedframes * np.hamming(flen)
@@ -16,7 +19,7 @@ def energy(frames):
     return np.sum(sqr_wnds, axis=1)
 
 
-def zcr(frames):
+def zcr(frames, fs=16000):
     """ Computes the short-time zcr of the given frames
 
     Args:
@@ -26,10 +29,13 @@ def zcr(frames):
         short-time zero-crossin rate of each frame
     """
     fixedframes = _fix_frames(frames)
-    nframes, flen = fixedframes.shape
+    fstride = frame.stride(fs)
     sgns = _sign(fixedframes)
-    abs_diff = np.abs(sgns[:, 1:] - sgns[:, :-1])
-    zcrs = (1 / (2*flen)) * np.sum(abs_diff, axis=1)
+    sample_before = np.roll(sgns[:, fstride - 1, None], 1)
+    sample_before[0] = sgns[0, 0]
+    shifted_sgns = np.hstack((sample_before, sgns[:, :-1]))
+    abs_diff = np.abs(sgns - shifted_sgns)
+    zcrs = (1 / (2*frames.shape[1])) * np.sum(abs_diff, axis=1)
     return zcrs
 
 
@@ -41,6 +47,7 @@ def _sign(X):
 
 
 def autocorr_norm(frames, lag=1):
+    """ Computes the normalised autocorrelation """
     corr, wnd_frames = autocorr(frames, lag)
     sqr_frames = wnd_frames ** 2
     sum1 = np.sum(sqr_frames[:, :-lag], axis=1)

tests/tests_shorttime.py
@@ -6,7 +6,7 @@ import math
 import numpy as np
 from scipy.io import wavfile
 
-from tests import signal01
+from tests import signal01, fs1
 from .context import pyspeech
 from pyspeech.conf import framing
 from pyspeech.dsp import shorttime 
@@ -43,12 +43,12 @@ class TestsShorttime(unittest.TestCase):
         amps = [0] * 10
         frames = frame.apply(Signal(amps, 5))
         lenergies = shorttime.log_energy(frames)
-        all50 = all(legy == -50 for legy in lenergies)
+        all50 = all(legy == -156.53559774527022 for legy in lenergies)
         self.assertTrue(all50)
 
     def test_zrate_allpositive(self):
         frames = get_frames(signal01)
-        rates = shorttime.zcr(frames)
+        rates = shorttime.zcr(frames, fs1)
         allpositive = all(rate >= 0 for rate in rates)
         self.assertTrue(allpositive)
 
@@ -57,10 +57,10 @@ class TestsShorttime(unittest.TestCase):
         amps = np.array([3, 3, 3, -3, -4, -5, 10, 15, 12, -1])
         fs = 5
         frames = frame.apply(Signal(amps, fs))
-        reals = [0, 1/3, 1/3, 0, 1/3, 1/3, 0, 1/3]
-        zerocross = shorttime.zcr(frames)
-        equal = all(rate == real for rate, real in zip(zerocross, reals))
-        self.assertTrue(equal)
+        reals = [0, 0.33, 0.33, 0.33, 0.33, 0.33, 0.33, 0.33]
+        zerocross = shorttime.zcr(frames, fs)
+        abs_err = np.abs(np.array(reals) - zerocross)
+        self.assertTrue(all(abs_err <= 0.01))
 
     def test_zcr_nocrossing_neg(self):
         allzero = self.zcr_nocrossing(-1)
@@ -75,7 +75,7 @@ class TestsShorttime(unittest.TestCase):
         _configure_frame()
         signal = Signal([amp]*10, 5)
         frames = frame.apply(signal)
-        zerocross = shorttime.zcr(frames)
+        zerocross = shorttime.zcr(frames, 5)
         allzero = all(zcr == 0 for zcr in zerocross)
         return allzero
 