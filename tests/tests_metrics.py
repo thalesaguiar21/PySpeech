54bbe01e53041f78e087f7538fbf6cc1559fa60e
Thales Aguiar <thalesaguiar21@gmail.com>
2020-07-23 16:09:16 -0300

    Updated metrics modules tests

--- tests/tests_metrics.py
+++ tests/tests_metrics.py
@@ -7,15 +7,15 @@ import pyspeech.dsp.metrics as spmet
 class TestsMetrics(unittest.TestCase):
 
     def test_m2hz(self):
-        mel = spmet.hz_to_mel(8000)
-        self.assertAlmostEqual(mel, 2840.023046708319)
+        mel = spmet.hz2mel(8000)
+        self.assertAlmostEqual(mel, 2840.02, 1)
 
     def test_hz2m(self):
-        hz = spmet.mel_to_hz(700)
-        self.assertAlmostEqual(hz, 602.709434226635865)
+        hz = spmet.mel2hz(700)
+        self.assertAlmostEqual(hz, 602.70, 1)
 
     def test_twoway_hz2mel_mel2hz(self):
-        mel = spmet.hz_to_mel(8000)
-        hz = spmet.mel_to_hz(mel)
+        mel = spmet.hz2mel(8000)
+        hz = spmet.mel2hz(mel)
         self.assertAlmostEqual(8000, hz)
 
